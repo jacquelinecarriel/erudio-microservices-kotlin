c8078aa4d97242c860c5120d1677111aa5b01f55
Jacqueline Carriel <jacqueline.carriell@gmail.com>
2025-11-07 13:07:20 -0300

    adjust files and add math controller

---
+++ src/main/kotlin/br/com/erudio/MathController.kt
@@ -0,0 +1,31 @@
+package br.com.erudio
+
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+class MathController {
+    val counter: AtomicLong = AtomicLong()
+
+    @RequestMapping(value = ["/sum/{numberOne}/{numberTwo}"])
+    fun sum(
+        @PathVariable(value= "numberOne")
+        numberOne: String?,
+        @PathVariable(value= "numberTwo")
+        numberTwo: String?
+
+
+    ): Double{
+
+//        if (!isNumeric(numberOne))
+        return 1.0
+    }
+
+//    private fun isNumeric (numberOne: String?): Any{
+//
+//    }
+
+}
