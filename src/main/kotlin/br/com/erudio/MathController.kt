ce030ebcac715d991f58496264f5f0935f780a48
Jacqueline Carriel <jacqueline.carriell@gmail.com>
2025-11-13 09:39:46 -0300

    add another operations

--- src/main/kotlin/br/com/erudio/MathController.kt
+++ src/main/kotlin/br/com/erudio/MathController.kt
@@ -13,17 +13,60 @@ class MathController {
 
     @RequestMapping(value = ["/sum/{numberOne}/{numberTwo}"])
     fun sum(
-        @PathVariable(value= "numberOne")
+        @PathVariable(value = "numberOne")
         numberOne: String?,
-        @PathVariable(value= "numberTwo")
+        @PathVariable(value = "numberTwo")
         numberTwo: String?
-
-
-    ): Double{
-
+    ): Double {
         if (!isNumeric(numberOne) || !isNumeric(numberTwo))
             throw UnsupportedOperationException("Please seth a numeric value!")
         return convertToDouble(numberOne) + convertToDouble(numberTwo)
+    }
+
+    @RequestMapping(value = ["/subtraction/{numberOne}/{numberTwo}"])
+    fun subtraction(
+        @PathVariable(value = "numberOne")
+        numberOne: String?,
+        @PathVariable(value = "numberTwo")
+        numberTwo: String?
+    ): Double {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw UnsupportedOperationException("Please seth a numeric value!")
+        return convertToDouble(numberOne) - convertToDouble(numberTwo)
+    }
+
+    @RequestMapping(value = ["/mean/{numberOne}/{numberTwo}"])
+    fun mean(
+        @PathVariable(value = "numberOne")
+        numberOne: String?,
+        @PathVariable(value = "numberTwo")
+        numberTwo: String?
+    ): Double {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw UnsupportedOperationException("Please seth a numeric value!")
+        return (convertToDouble(numberOne) - convertToDouble(numberTwo)) / 2
+    }
+
+    @RequestMapping(value = ["/division/{numberOne}/{numberTwo}"])
+    fun division(
+        @PathVariable(value = "numberOne")
+        numberOne: String?,
+        @PathVariable(value = "numberTwo")
+        numberTwo: String?
+    ): Double {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw UnsupportedOperationException("Please seth a numeric value!")
+        return convertToDouble(numberOne) / convertToDouble(numberTwo)
+    }
+
+    @RequestMapping(value = ["/squareRoot/{number}"])
+    fun squareRoot(
+        @PathVariable(value = "number")
+        number: String?,
+    ): Double {
+        if (!isNumeric(number))
+            throw UnsupportedOperationException("Please seth a numeric value!")
+        return Math.sqrt(convertToDouble(number))
     }
 
     private fun convertToDouble(strNumber: String?): Double {
@@ -32,10 +75,17 @@ class MathController {
         return if (isNumeric(number)) number.toDouble() else 0.0
     }
 
-    private fun isNumeric (strNumber: String?): Boolean{
+    private fun isNumeric(strNumber: String?): Boolean {
         if (strNumber.isNullOrBlank()) return false
         val number = strNumber.replace(",".toRegex(), ".")
         return number.matches("""[-+]?[0-9]*\.?[0-9]+""".toRegex())
     }
 
+
 }
+
+
+
+
+
+
